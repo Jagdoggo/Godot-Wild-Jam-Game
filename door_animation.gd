38ea700af5ce8ec10093f870b0a4b761a0c5d5be
Jagdoggo <emlynharrison2000@gmail.com>
2026-08-16 16:30:53 +1200

    Added stamina and bow enemy(cant shoot)
    The bow enemy is placeholder art with no animations

--- door_animation.gd
+++ door_animation.gd
@@ -1,9 +1,11 @@
 extends AnimatedSprite2D
 
 @onready var player: CharacterBody2D = $"../Player"
+@onready var break_particles: CPUParticles2D = $"DoorStaticBody/Break Particles"
 
 func _on_door_area_body_entered(body: Node2D) -> void:
 	if player.inputC:
 		play()
+		break_particles.emitting = true
 		await animation_finished
 		$DoorStaticBody.queue_free()
