5354fb1bb9bc8621d136ca393af4203aa0c4693c
Jagdoggo <emlynharrison2000@gmail.com>
2026-08-18 16:27:42 +1200

    invicible

--- arrow.gd
+++ arrow.gd
@@ -9,6 +9,8 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body == self:
 		return
-	if body.name == "Player":
+	if body.name == "Player" and !body.invincible:
 		body.health -= damage
+		body.invincible = true
+		body.invincible_timer.start()
 	queue_free()
