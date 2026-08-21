6f59e15aa385c331004c7bb99259b89b3ad4d9c4
TheSniperKing302 <isaacreid7343@gmail.com>
2026-08-21 20:16:24 +1200

    Button Shenanigans
    Shenanigans?!

--- next_castle_ui.gd
+++ next_castle_ui.gd
@@ -4,3 +4,7 @@ func _on_button_pressed() -> void:
 	Global.dungeon_level += 1
 	print(Global.dungeon_level)
 	get_tree().change_scene_to_file("res://castle.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
